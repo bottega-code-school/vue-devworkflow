e5ac81973142ab886746fce013252b4cb50de903
jordanhudgens <jordan@devcamp.com>
2018-09-25 13:27:27 -0500

    A_04_creating_devworkflow_views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Greeting msg="Welcome to Your Vue.js App" />
+    Homepage
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Greeting from '@/components/Greeting.vue'
 
 export default {
   name: 'home',
-  components: {
-    Greeting
-  }
 }
 </script>
--- src/views/ProjectDashboard.vue
+++ src/views/ProjectDashboard.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>Updated about page</h1>
+    <h1>Project dashboard</h1>
   </div>
 </template>
